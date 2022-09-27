a4b857daea4e1f44aba9bcf57814652f52d696bb
LP <grofit1@yahoo.com>
2022-09-27 12:50:10 +0100

    Have updated plugin output style
    This aligns better with many plugins and also now includes all required dlls

--- src/Strem.Build/Tasks/PackagePluginTask.cs
+++ src/Strem.Build/Tasks/PackagePluginTask.cs
@@ -2,6 +2,7 @@
 using Cake.Common.Tools.DotNet;
 using Cake.Common.Tools.DotNet.Build;
 using Cake.Common.Tools.DotNet.MSBuild;
+using Cake.Common.Tools.DotNet.Publish;
 using Cake.Frosting;
 
 namespace Strem.Build.Tasks;
@@ -11,21 +12,30 @@ public class PackagePluginTask : FrostingTask<BuildContext>
 {
     public override void Run(BuildContext context)
     {
-        var outputDirectory = $"{Directories.Dist}/Strem.OBS.v4-{context.Version}";
-        if(!Directory.Exists(outputDirectory))
-        { context.CreateDirectory(outputDirectory); }
+        var pluginName = $"Strem.OBS.v4";
+        var pluginTempOutput = $"{Directories.Dist}/plugin-temp";
+        var pluginContainerFolder = $"{Directories.Dist}/plugin/";
+        var pluginFinalOutput = $"{pluginContainerFolder}/{pluginName}";
+        if(!Directory.Exists(pluginTempOutput)) { context.CreateDirectory(pluginTempOutput); }
+        if(!Directory.Exists(pluginFinalOutput)) { context.CreateDirectory(pluginFinalOutput); }
 
         var pluginProject = $"{Directories.Src}/Strem.OBS.v4/Strem.OBS.v4.csproj";
-        var publishSettings = new DotNetBuildSettings
+        var publishSettings = new DotNetPublishSettings
         {
             Configuration = "Release",
-            OutputDirectory = outputDirectory,
+            OutputDirectory = pluginTempOutput,
             MSBuildSettings = new DotNetMSBuildSettings
             {
                 Version = context.Version
-           }
+            }
         };
-        context.DotNetBuild(pluginProject, publishSettings);
-        context.Zip(outputDirectory, $"{outputDirectory}.zip");
+        context.DotNetPublish(pluginProject, publishSettings);
+
+        context.MoveFile($"{pluginTempOutput}/Obs.v4.WebSocket.dll", $"{pluginFinalOutput}/Obs.v4.WebSocket.dll");
+        context.MoveFile($"{pluginTempOutput}/Obs.v4.WebSocket.Reactive.dll", $"{pluginFinalOutput}/Obs.v4.WebSocket.Reactive.dll");
+        context.MoveFile($"{pluginTempOutput}/WebSocket4Net.dll", $"{pluginFinalOutput}/WebSocket4Net.dll");
+        context.MoveFile($"{pluginTempOutput}/SuperSocket.ClientEngine.dll", $"{pluginFinalOutput}/SuperSocket.ClientEngine.dll");
+        context.MoveFile($"{pluginTempOutput}/Strem.OBS.v4.dll", $"{pluginFinalOutput}/Strem.OBS.v4.dll");
+        context.Zip(pluginContainerFolder, $"{Directories.Dist}/{pluginName}.zip");
     }
 }
